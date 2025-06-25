e26931148e0d38c3ffb0dcb5f5bc53d9d19b88ed
KoreshAleh <youdive8@gmail.com>
2025-06-25 17:39:48 +0200

    обновил стили и добавил адаптивность

--- src/commponets/ToDoEdit.jsx
+++ src/commponets/ToDoEdit.jsx
@@ -12,8 +12,11 @@ function ToDoEdit({editText, handelEditInput, saveEdit}) {
             <div className="up-side">
             <h1>TO-DO App</h1>
             <h2 id="save-text">Create a new text</h2>
-            <input type="text" placeholder='Введите текст' onChange={handelEditInput} value={editText} />
+            <div className="input-button-wrapper">
+                <input type="text" placeholder='Введите текст' onChange={handelEditInput} value={editText} />
              <button id="save" onClick={saveEdit}>Save</button>
+            </div>
+                
           </div> 
         </>
      );
